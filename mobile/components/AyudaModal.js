983cf98f268886b3715e41beced4c24e7e67ed50
OdemaConsultingAI <ag@odemaconsulting.com>
2026-01-31 17:34:20 -0500

    Ajuste a modulo de ayuda

--- mobile/components/AyudaModal.js
+++ mobile/components/AyudaModal.js
@@ -88,7 +88,7 @@ export default function AyudaModal() {
               ))}
             </ScrollView>
           </SafeAreaView>
-        </View>
+        </Pressable>
       </Pressable>
     </Modal>
   );
